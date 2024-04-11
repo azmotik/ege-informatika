88238af8d886fb06cd8b7b0d846f2d7265245497
Mukhutdinov Timerkhan <t.mukhutdinov.job@gmail.com>
2024-04-11 15:39:13 +0300

    решение задач 26 с занятия

--- 26/26.py
+++ 26/26.py
@@ -87,3 +87,127 @@ for i in data[1:]:
     if arr[-1] - i >= 7: 
         arr.append(i)
 print(len(arr), arr[-1])
+
+
+
+#TODO: на занятии решали
+'''
+# 26var079
+file = open('26var09.txt').readlines()
+N = int(file[0])
+tuples = []
+for i in range(1, N + 1):
+    arr = file[i].split(' ')
+    tuples.append((int(arr[0]), int(arr[1])))
+
+tuples = sorted(tuples, key=lambda item: (item[0], item[1]), reverse=True)
+
+result_arr = []
+for i in range(len(tuples)-1):
+    if int(tuples[i][1]) - int(tuples[i+1][1]) == 3:
+        result_arr.append(tuples[i+1])
+
+max_arr = max(result_arr, key=lambda t: t[0])
+
+rrr = filter(lambda item: item[0] == max_arr[0], result_arr)
+min_arr = min(rrr, key=lambda t: t[1])
+
+print(result_arr)
+print(max_arr[0], min_arr[1] + 1)
+print(N)
+
+# 26var07
+file = open('26var07.txt').readlines()
+
+n = int(file[0].split('\t')[0])
+m = int(file[0].split('\t')[1])
+arr_n = []
+arr_m = []
+for i in range(n):
+    arr_n.append(int(file[i].split('\t')[0]))
+
+for i in range(m):
+    arr_m.append(int(file[i].split('\t')[1]))
+
+arr_n.sort()
+arr_m.sort()
+
+arr_sorted = []
+for i in range(m):
+    if arr_m[i] in arr_n:
+        arr_sorted.append(arr_m[i])
+        arr_n.remove(arr_m[i])
+
+max_res = 0
+max_len = 0
+for j in range(0, 50):
+    arr1 = [arr_sorted[j]]
+    for i in range(len(arr_sorted)):
+        if arr_sorted[i] - arr1[-1] >= 6:
+            arr1.append(arr_sorted[i])
+    max_res = max(max_res, len(arr1))
+    if max_res == len(arr1):
+        max_len = max(max_len, arr_sorted[j])
+
+
+print(arr_n)
+print(arr_m)
+print(arr_sorted)
+
+print(n, m)
+print(max_res, max_len)
+
+
+# 26var06
+file = open('26var06.txt').readlines()
+arr = [int(x) for x in file]
+n = int(arr[0])
+arr = arr[1:]
+
+arr.sort()
+
+max_res = 0
+max_len = 0
+for j in range(0, 50):
+    arr1 = [arr[j]]
+    for i in range(len(arr)):
+        if arr[i] - arr1[-1] >= 6:
+            arr1.append(arr[i])
+    max_res = max(max_res, len(arr1))
+    if max_res == len(arr1):
+        max_len = max(max_len, arr[j])
+
+print(max_res, max_len)
+print(arr)
+
+
+file = open('26.7_13397.txt').readlines()
+N = int(file[0])
+tuples = []
+for i in range(1, N + 1):
+    arr = file[i].split(' ')
+    tuples.append((int(arr[0]), int(arr[1]), int(arr[0]) + int(arr[1])))
+
+tuples = sorted(tuples, key=lambda item: item[0])
+
+max_time=max(tuples, key=lambda t: t[0])
+
+arr = []
+first = min(tuples, key=lambda t: t[2])
+arr.append(first)
+
+for i in range(N):
+    arr_sort = filter(lambda item: item[0] >= first[2], tuples)
+    first = min(arr_sort, key=lambda t: t[2])
+    arr.append(first)
+    if first[2] > max_time[0]:
+        break
+
+tuples_6 = sorted(tuples, key=lambda item: item[2])
+
+print(arr)
+print(tuples)
+print(tuples_6)
+
+print(len(arr))
+'''
