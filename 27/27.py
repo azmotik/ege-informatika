36f73f033e56f5c66e4dc87824b9ca0d0e96ac68
Mukhutdinov Timerkhan <t.mukhutdinov.job@gmail.com>
2024-04-18 15:36:43 +0300

    Заянтие 18_04_2024

--- 27/27.py
+++ 27/27.py
@@ -1,4 +1,21 @@
-# 27_17
+# 27_1
+file = open('27v01_A.txt').readlines()
+K = int(file[0])
+N = int(file[1])
+
+min1 = 11000000000000000
+min2 = 11000000000000000
+min3 = 11000000000000000
+
+for i in range(2, N + 2 - (K * 2)):
+    min1 = min(min1, int(file[i]))
+    min2 = min(min2, min1 + int(file[i + K]))
+    min3 = min(min3, min2 + int(file[i + K * 2]))
+
+print(min3)
+
+
+# 27_17a
 file = open('27/27v17_A.txt').readlines()
 N = int(file[0])
 arr = []
@@ -9,4 +26,36 @@ for i in range(1, N + 1):
     arr.append(max_arr1)
 
 print(sum(arr), sum(arr) % 39 != 0)
+
+# 27_17a_b
+file = open('27v17_B.txt').readlines()
+N = int(file[0])
+arr = []
+
+for i in range(1, N + 1):
+    arr1 = file[i].split(' ')
+    max_arr1 = max(int(arr1[0]), int(arr1[1]))
+    arr.append(max_arr1)
+
+
+arr_sum = sum(arr)
+for j in range(1, N + 1):
+    min_razn = 600000
+
+    for i in range(1, N + 1):
+        arr1 = file[i].split(' ')
+        razn = abs(int(arr1[0]) - int(arr1[1]))
+
+        if razn != 0 and razn % 39 != 0:
+            min_razn = min(min_razn, razn)
+
+    #print(min_razn, arr_sum - min_razn, ( arr_sum - min_razn ) % 39 == 0)
+    if arr_sum % 39 == 0:
+        if (arr_sum - min_razn) % 39 != 0:
+            print(min_razn, arr_sum - min_razn, (arr_sum - min_razn) % 39 == 0)
+            break
+    else:
+        print(arr_sum, arr_sum % 39 == 0)
+        break
+
 
